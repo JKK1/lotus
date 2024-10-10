930d0a6b77653cfb2dcaefdef377e6c93227b217
Phi <orjan.roren@gmail.com>
2024-10-10 13:46:58 +0200

    chore: set GenesisNetworkVersion to Version23
    chore: set GenesisNetworkVersion to Version23

--- build/buildconstants/params_butterfly.go
+++ build/buildconstants/params_butterfly.go
@@ -18,7 +18,7 @@ var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandQuicknet,
 }
 
-const GenesisNetworkVersion = network.Version22
+const GenesisNetworkVersion = network.Version23
 
 var NetworkBundle = "butterflynet"
 var ActorDebugging = false
